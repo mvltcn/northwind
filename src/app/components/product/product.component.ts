d9157151971c7d534cfd72d340aa109ed81c668a
Mevlüt <mevlutcan@outlook.com>
2021-12-06 14:21:51 +0300

    18.gün angular

--- src/app/components/product/product.component.ts
+++ src/app/components/product/product.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { Product } from 'src/app/models/product';
 import { HttpClient } from '@angular/common/http';
 import { ProductService } from 'src/app/services/product.service';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-product',
@@ -12,10 +13,17 @@ export class ProductComponent implements OnInit {
   products: Product[] = [];
   dataLoaded=false;
 
-  constructor(private productService: ProductService) {}
+  constructor(private productService: ProductService,private activetedRoute:ActivatedRoute) {}
 
   ngOnInit(): void {
-    this.getProducts();
+    this.activetedRoute.params.subscribe(params=>{
+      if (params["categoryId"]) {
+        this.getProductsByCategory(params["categoryId"])
+      }else{
+        this.getProducts()
+      }
+    })
+    this
   }
 
   getProducts() {
@@ -23,6 +31,13 @@ export class ProductComponent implements OnInit {
       this.products=response.data
       this.dataLoaded=true;
     })
-    console.log();
+    
+  }
+  getProductsByCategory(categoryId:number) {
+    this.productService.getProductsByCategory(categoryId).subscribe(response=>{
+      this.products=response.data
+      this.dataLoaded=true;
+    })
+    
   }
 }
